60ca409ff805543bea7fde432b708ea6d6aad23c
Ruslan Khaibaliev <nastycoocher@gmail.com>
2024-12-01 22:38:33 +0300

    ”add”

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: "/ResultsOfOpros/", // Замените <REPO> на имя вашего репозитория
+  base: "/ResultsOfOprosNow/", // Замените <REPO> на имя вашего репозитория
 });
